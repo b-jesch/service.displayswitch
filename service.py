0218ad75a9641db111f479f651d97da3c26e7068
Birger Jesch <birger.jesch@gmail.com>
2020-12-10 20:27:27 +0100

    introduce settings.xml

--- service.py
+++ service.py
@@ -5,7 +5,6 @@ import time
 import sys
 import os
 import subprocess
-import filecmp
 
 import xbmc
 import xbmcaddon
@@ -14,13 +13,13 @@ import xbmcvfs
 
 from resources.lib.tools import OsRelease, KodiLib
 
-addon = xbmcaddon.Addon()
+addon = xbmcaddon.Addon(id='service.displayswitch')
 addon_path = xbmc.translatePath(addon.getAddonInfo('path'))
 addon_profile = xbmc.translatePath(addon.getAddonInfo('profile'))
 LOC = addon.getLocalizedString
 
-display_config_path = '/flash/config.txt'
-display_config_backup = '/flash/config.txt.origin'
+system_config_path = '/flash/config.txt'
+system_config_backup = '/flash/config.txt.origin'
 
 addon_config_path = os.path.join(addon_path, 'resources', 'configs')
 user_config_path = os.path.join(addon_profile, 'configs')
@@ -41,9 +40,9 @@ if not os.path.exists(user_config_path):
     xbmcvfs.copy(os.path.join(addon_config_path, user_config_1), os.path.join(user_config_path, user_config_1))
     xbmcvfs.copy(os.path.join(addon_config_path, user_config_2), os.path.join(user_config_path, user_config_2))
 
-if not os.path.exists(display_config_backup):
+if not os.path.exists(system_config_backup):
     subprocess.call(['mount', '-o', 'remount,rw', '/flash'], shell=False)
-    xbmcvfs.copy(display_config_path, display_config_backup)
+    xbmcvfs.copy(system_config_path, system_config_backup)
     subprocess.call(['mount', '-o', 'remount,ro', '/flash'], shell=False)
 
 # GPIO-Port, an dem die Taste gegen GND angeschlossen ist
@@ -57,21 +56,6 @@ SHUTDOWN = 3
 DEBOUNCE = 0.05
 
 duration = 0
-
-# Umkopieren der Konfigurationsdateien von /storage/.kodi/userdata/addon_data/service.displayswitcher/configs/
-# nach /flash/config.txt
-
-def switch_config():
-
-    subprocess.call(['mount', '-o', 'remount,rw', '/flash'], shell=False)
-
-    if filecmp.cmp(display_config_path, os.path.join(user_config_path, user_config_1), shallow=False):
-        changed = xbmcvfs.copy(os.path.join(user_config_path, user_config_2), display_config_path)
-    else:
-        changed = xbmcvfs.copy(os.path.join(user_config_path, user_config_1), display_config_path)
-
-    subprocess.call(['mount', '-o', 'remount,ro', '/flash'], shell=False)
-    return changed
 
 
 # Interrupt-Routine für den Button
@@ -91,25 +75,34 @@ def buttonISR(pin):
             duration = 0
 
             if elapsed > SHUTDOWN:
+
                 kl.log('initiate shutdown')
                 kl.notify(LOC(32010), LOC(32023))
                 xbmc.sleep(5000)
                 xbmc.executebuiltin('Powerdown')
-                # subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
-            elif elapsed > DEBOUNCE:
+            if elapsed > DEBOUNCE:
+
                 kl.log('initiate configuration change and reboot')
 
-                if switch_config():
+                curr = addon.getSetting('current')
+
+                if curr == user_config_1:
+                    src = user_config_2
+                else:
+                    src = user_config_1
+
+                subprocess.call(['mount', '-o', 'remount,rw', '/flash'], shell=False)
+                changed = xbmcvfs.copy(os.path.join(user_config_path, src), system_config_path)
+                addon.setSetting('current', src)
+                subprocess.call(['mount', '-o', 'remount,ro', '/flash'], shell=False)
+
+                if changed:
                     kl.notify(LOC(32010), LOC(32022))
                     xbmc.sleep(5000)
                     xbmc.executebuiltin('Reboot')
-                    # subprocess.call(['shutdown', '-r', 'now'], shell=False)
                 else:
                     kl.notify(LOC(32010), LOC(32021), xbmcgui.NOTIFICATION_ERROR)
-
-            else:
-                pass
 
 
 if __name__ == '__main__':
@@ -118,7 +111,6 @@ if __name__ == '__main__':
 
     # GPIO initialisieren, BMC-Pinnummer, Pullup-Widerstand
 
-    GPIO.cleanup(PORT)
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PORT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -136,4 +128,4 @@ if __name__ == '__main__':
 
     GPIO.setmode(GPIO.BCM)
     GPIO.remove_event_detect(PORT)
-
+    GPIO.cleanup(PORT)
